0a80b5eb29c0a6985f53b12e5baba8e75fb54934
zengge <gzeng@thoughtworks.com>
2019-08-09 17:56:57 +0800

    kotlin: function

--- kotlin-study/src/org/gzeng/funs/declare.kt
+++ kotlin-study/src/org/gzeng/funs/declare.kt
@@ -7,3 +7,16 @@ fun myPrint(str: String): Unit{
 private fun getCount(): Int = 1
 
 internal fun getName(): String = "sky"
+
+
+fun defaultParameters(name: String, age: Int = 20): Unit {
+    println("name: $name, age: $age")
+}
+
+fun namedParameters(name: String, address: String = "xi'an", age: Int) {
+    println("name: $name, address: $address, age: $age")
+}
+
+fun <T> variableParameters(name: String, vararg params: T) {
+    println("name: $name, params size: ${params.size}")
+}
--- kotlin-study/src/org/gzeng/funs/run.kt
+++ kotlin-study/src/org/gzeng/funs/run.kt
@@ -2,4 +2,9 @@ package org.gzeng.funs
 
 fun main() {
     myPrint("hello function")
+    defaultParameters("sky")
+    namedParameters("sky", age = 20);
+    namedParameters(age = 20, name = "sky");
+
+    variableParameters("sky", "xi'an", "huanpu", 123)
 }
